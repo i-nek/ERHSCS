cbfb7211b9e430cc8d5a8c71afd835ca06b913fa
KuyaKen <ricewitduhspringroll@gmail.com>
2024-10-10 19:30:57 -0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,51 @@
+# Keywords
+# False
+# True
+# None
+# and
+# break
+# def
+# if
+# elif
+# else
+# return
+# while
+# or
+# not
+
+# conditionals
+# if/elif/else
+# conditions
+
+num1 = 5
+num2 = 2
+x = 5 < 2 # false
+
+def foo():
+    if (x) :
+        return True
+    else:
+        return False
+def foo2():
+    if (x):
+        return True
+    elif not (x):
+        return False
+
+print(x)
+print(foo2())
+
+
+x = 5 > 2 # true
+
+if (x) :
+    return True
+else:
+    return False
+
+
+if (x):
+    return True
+elif not (x):
+    return False
+        
