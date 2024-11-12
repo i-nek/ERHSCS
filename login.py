b9c54472810c2eafec7fa96dc6492617c65b309f
KuyaKen <92195844+i-nek@users.noreply.github.com>
2024-11-12 08:32:29 -0800

    Update login.py

--- login.py
+++ login.py
@@ -1,16 +1,20 @@
 db = {
-   'kenneth': 'password123' 
+   'kenneth': 'password123',
+   'kenneth1': 'password'
 }
 
 def checkPassword(username, password):
-    if not (username in db or password in db):
-        print("Incorrect Username or Password.")
+    if not (username in db):
+        print("Username not found")
         return
+    for i, v in db.items():
+        if (i == username):
+            if not(v == password):
+                print("Incorrect Password")
+                return
     print("You have successfully logged in.")
 
 username = (input("Username: ")).lower() # you can add .lower() to make the input all lowercase.
 password = input("Password: ")
 
 checkPassword(username, password)
-
-
